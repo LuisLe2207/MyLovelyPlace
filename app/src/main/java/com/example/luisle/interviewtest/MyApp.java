552981dacd819ec35c109414cfff8a631427abba
LuisLe2207 <khoa.led2207@gmail.com>
2017-06-28 09:22:42 +0700

    Add some codes

--- app/src/main/java/com/example/luisle/interviewtest/MyApp.java
+++ app/src/main/java/com/example/luisle/interviewtest/MyApp.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.example.luisle.interviewtest.data.source.di.DaggerPlacesRepositoryComponent;
 import com.example.luisle.interviewtest.data.source.di.PlacesRepositoryComponent;
+import com.example.luisle.interviewtest.data.source.di.PlacesRepositoryModule;
 
 /**
  * Created by LuisLe on 6/27/2017.
@@ -18,6 +19,7 @@ public class MyApp extends Application {
         super.onCreate();
 
         repositoryComponent = DaggerPlacesRepositoryComponent.builder()
+                .placesRepositoryModule(new PlacesRepositoryModule())
                 .appModule(new AppModule((getApplicationContext())))
                 .build();
 
